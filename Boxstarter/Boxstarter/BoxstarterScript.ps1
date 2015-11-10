77a6327caed07e8f14b709130be0a1d3c43e1be4
Daniel Martin <danm@solidroute.com>
2015-11-10 14:35:07 +0000

    Commit first early draft

---
+++ Boxstarter/Boxstarter/BoxstarterScript.ps1
@@ -0,0 +1,24 @@
+﻿#
+# BoxstarterScript.ps1
+#
+
+# Boxstarter options
+$Boxstarter.RebootOk=$true # Allow reboots?
+$Boxstarter.NoPassword=$false # Is this a machine with no login password?
+$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
+
+# Basic setup - need to verify each of these commands
+Update-ExecutionPolicy Unrestricted
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Enable-RemoteDesktop
+
+Set-TaskbarSmall # depricated look up Set-TaskbarOptions. NOt sure if liked
+
+if (Test-PendingReboot) { Invoke-Reboot }
+
+# Update Windows and reboot if necessary
+Install-WindowsUpdate -AcceptEula
+if (Test-PendingReboot) { Invoke-Reboot }
+
+
+
